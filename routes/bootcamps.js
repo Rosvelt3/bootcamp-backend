2a626a825a938c549c2cbc8d736c8d53442f03f4
isaias005 <isaias005@hotmail.com>
2020-01-20 20:43:19 -0400

    Everything

---
+++ routes/bootcamps.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+const {
+    getBootcamps,
+    getBootcamp,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp,
+    getBootcampsInRadius,
+    bootcampPhotoUpload
+} = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+
+const advancedResults = require('../middleware/advancedResults');
+
+const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
+
+const { protect, authorize } = require('../middleware/auth');
+
+router.use('/:bootcampId/courses', courseRouter)
+router.use('/:bootcampId/reviews', reviewRouter)
+
+router.route('/radius/:zipcode/:distance')
+    .get(getBootcampsInRadius);
+
+router.route('/')
+    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+    .post(protect, authorize('publisher', 'admin'), createBootcamp)
+
+router.route('/:id')
+    .get(getBootcamp)
+    .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+    .delete(protect, authorize('publisher', 'admin'), deleteBootcamp)
+
+router.route('/:id/photo')
+    .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,58 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const colors = require('colors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const errorHandler = require('./middleware/error')
+const connectDB = require('./config/db');
+
 require('dotenv').config({ path: './config/config.env' });
+connectDB();
+
+const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
+const auth = require('./routes/auth')
+const users = require('./routes/users')
+const reviews = require('./routes/reviews')
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(rateLimit({windowMs: 10 * 60 * 1000, max:100}));
+app.use(hpp());
+app.use(cors());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+const server = app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold);
 });
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+})
