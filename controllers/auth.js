edaac2c4c820e36062efa456e5c94956770dab71
Isaias <isaias005@hotmail.com>
2021-11-18 19:52:43 -0400

    cors test

--- controllers/auth.js
+++ controllers/auth.js
@@ -171,8 +171,8 @@ const sendTokenResponse = (user, statusCode, res) => {
     const options = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: true,
-        sameSite: 'none'
+        secure: false,
+        sameSite: 'lax'
     };
 
     if (process.env.NODE_ENV === 'production') {
