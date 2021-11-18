0329b1a9f4c400e6f20e1287d91ca56abf3dd7c6
Isaias <isaias005@hotmail.com>
2021-11-18 19:48:04 -0400

    cookie test

--- controllers/auth.js
+++ controllers/auth.js
@@ -171,8 +171,8 @@ const sendTokenResponse = (user, statusCode, res) => {
     const options = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax'
+        secure: true,
+        sameSite: 'none'
     };
 
     if (process.env.NODE_ENV === 'production') {
